8b9e9c282e6d6e4928d2766e6a1e96d3b5d13560
nabin <nkbhaie2017@gmail.com>
2024-03-27 10:44:09 -0600

    add first java class

---
+++ Class.java
@@ -0,0 +1,12 @@
+/* To Run your java file in termimal
+ * Open up the terminal
+ * cd into the project folder
+ * run these commands:  javac FileName.java
+ *                      java FileName -> this is bytecode
+ */
+
+public class Class{
+    public static void main(String[]args){
+        System.out.println("First Java Class");
+    }
+}
